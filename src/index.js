c0afa080bbdd55861f2a8a06ef2c6ab49f61f2e8
Luca Bacchi <bacchilu@gmail.com>
2020-11-28 14:35:47 +0000

    UI refactorings

--- src/index.js
+++ src/index.js
@@ -1,20 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import hdate from 'human-date';
 
-const getData = function (hitsPerPage = 20) {
+const ajaxGet = async function (start, end, hitsPerPage) {
+    const url = `https://hn.algolia.com/api/v1/search?query=&numericFilters=created_at_i>${start},created_at_i<=${end},points>=100&hitsPerPage=${hitsPerPage}`;
+    return await (await fetch(url)).json();
+};
+
+const getData = async function () {
     const NOW = new Date() / 1000;
     const DAY = 60 * 60 * 24;
     const start = NOW - 3 * DAY;
     const end = NOW;
-    const url = `https://hn.algolia.com/api/v1/search?query=&numericFilters=created_at_i>${start},created_at_i<=${end},points>=100&hitsPerPage=${hitsPerPage}`;
-    return fetch(url)
-        .then(function (res) {
-            return res.json();
-        })
-        .then(function (res) {
-            if (res['nbHits'] > res['hitsPerPage']) return getData(res['nbHits']);
-            return res['hits'];
-        });
+    const res = await ajaxGet(start, end, 0);
+    return (await ajaxGet(start, end, res['nbHits']))['hits'];
+};
+
+const relativeTime = function (createdAt) {
+    return hdate.relativeTime(new Date(createdAt));
+};
+
+const toLocaleString = function (createdAt) {
+    return new Date(createdAt).toLocaleString('en-US', {hour12: false});
+};
+
+const getColor = function (points) {
+    if (points >= 3200) return 'danger';
+    if (points >= 1600) return 'warning';
+    if (points >= 800) return 'success';
+    if (points >= 400) return 'primary';
+    if (points >= 200) return 'info';
+    return 'secondary';
 };
 
 const CardText = function ({item}) {
@@ -41,20 +57,18 @@ const Items = function ({items}) {
         return (
             <div key={item['objectID']} className="card border-dark bg-light m-3">
                 <div className="card-body">
-                    <h5 className="card-title">
-                        {item['title']}
-                        <span className="badge badge-info float-right">{item['points']}</span>
-                    </h5>
+                    <h5 className="card-title">{item['title']}</h5>
                     <h6 className="card-subtitle mb-2 text-muted">
                         {item['author']}
-                        <em className="float-right">
-                            {new Date(item['created_at']).toLocaleString('en-US', {hour12: false})}
+                        <em className="float-right" title={toLocaleString(item['created_at'])}>
+                            {relativeTime(item['created_at'])}
                         </em>
                     </h6>
                     <CardText item={item} />
+                    <span className={`badge badge-${getColor(item['points'])}`}>{item['points']}</span>
                     <a
                         href={`https://news.ycombinator.com/item?id=${item['objectID']}`}
-                        className="card-link"
+                        className="card-link float-right"
                         target="_black"
                     >
                         {item['num_comments']} Comments
@@ -68,8 +82,7 @@ const Items = function ({items}) {
 const App = function (props) {
     const [items, setItems] = React.useState(null);
     React.useEffect(async function () {
-        const res = await getData();
-        setItems(res);
+        setItems(await getData());
     }, []);
 
     return <Items items={items} />;
