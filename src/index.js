38c9b2610ced08fe833cff321f962504e49a3055
Luca Bacchi <bacchilu@gmail.com>
2021-03-25 16:01:16 +0000

    Refactoring

--- src/index.js
+++ src/index.js
@@ -23,40 +23,43 @@ import {useUser, LoginButton} from './utils.js';
 //     return (await ajaxGet(start, end, res['nbHits']))['hits'];
 // };
 
-const ajaxGet = async function (start, end, hitsPerPage) {
-    const searchParams = new URLSearchParams({
-        query: '',
-        numericFilters: `created_at_i>${start},created_at_i<=${end}`,
-        hitsPerPage: hitsPerPage,
-    });
-    const url = `https://hn.algolia.com/api/v1/search?${searchParams.toString()}`;
-    return await (await fetch(url)).json();
-};
+const Fetch = (function () {
+    const ajaxGet = async function (start, end, hitsPerPage) {
+        const searchParams = new URLSearchParams({
+            query: '',
+            numericFilters: `created_at_i>${start},created_at_i<=${end}`,
+            hitsPerPage: hitsPerPage,
+        });
+        const url = `https://hn.algolia.com/api/v1/search?${searchParams.toString()}`;
+        return (await fetch(url)).json();
+    };
 
-const getDaysData = async function (start, end, n) {
-    return (await ajaxGet(start, end, n))['hits'];
-};
+    const getDaysData = async function (start, end, n) {
+        return (await ajaxGet(start, end, n))['hits'];
+    };
 
-const getData = async function () {
-    const NOW = new Date() / 1000;
-    const DAY = 60 * 60 * 24;
+    return {
+        getData: async function () {
+            const NOW = new Date() / 1000;
+            const DAY = 60 * 60 * 24;
 
-    const res = await Promise.all([
-        getDaysData(NOW - 7 * DAY, NOW - 6 * DAY, 1),
-        getDaysData(NOW - 6 * DAY, NOW - 5 * DAY, 2),
-        getDaysData(NOW - 5 * DAY, NOW - 4 * DAY, 4),
-        getDaysData(NOW - 4 * DAY, NOW - 3 * DAY, 8),
-        getDaysData(NOW - 3 * DAY, NOW - 2 * DAY, 16),
-        getDaysData(NOW - 2 * DAY, NOW - 1 * DAY, 32),
-        getDaysData(NOW - 1 * DAY, NOW - 0 * DAY, 64),
-    ]);
-    const r = [...res[0], ...res[1], ...res[2], ...res[3], ...res[4], ...res[5], ...res[6]];
-    return r.sort(function (a, b) {
-        if (a['points'] < b['points']) return 1;
-        if (a['points'] > b['points']) return -1;
-        return 0;
-    });
-};
+            const res = await Promise.all(
+                [0, 1, 2, 3, 4, 5, 6].map(function (i) {
+                    return getDaysData(NOW - (7 - i) * DAY, NOW - (6 - i) * DAY, 2 ** i);
+                })
+            );
+            return res
+                .reduce(function (acc, item) {
+                    return [...acc, ...item];
+                }, [])
+                .sort(function (a, b) {
+                    if (a['points'] < b['points']) return 1;
+                    if (a['points'] > b['points']) return -1;
+                    return 0;
+                });
+        },
+    };
+})();
 
 const Items = function ({items}) {
     if (items === null)
@@ -75,7 +78,7 @@ const Items = function ({items}) {
 const App = function (props) {
     const [items, setItems] = React.useState(null);
     React.useEffect(async function () {
-        setItems(await getData());
+        setItems(await Fetch.getData());
     }, []);
 
     return <Items items={items} />;
