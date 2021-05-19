40d411a89f264bb59e821720c0a3a858fe5d9e57
Luca Bacchi <bacchilu@gmail.com>
2021-05-19 13:32:10 +0000

    Refactoring CommentPage

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter as Router, Switch, Route, useParams} from 'react-router-dom';
-import useSWR from 'swr';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
+import {CommentPage} from './comment_page.js';
 
 import {Card} from './card.js';
 import {useUser, LoginButton, Spinner, Badge} from './utils.js';
@@ -27,56 +27,12 @@ const Main = function (props) {
     return <Items items={data} />;
 };
 
-const CardText = function ({item}) {
-    if (item['text'] !== undefined) return <em dangerouslySetInnerHTML={{__html: item['text']}}></em>;
-    return (
-        <p className="card-text text-truncate">
-            <a href={item['url']} target="_blank">
-                {item['url']}
-            </a>
-        </p>
-    );
-};
-
-const Comment = function (props) {
-    const {commentId} = useParams();
-    const {data, error} = useSWR(`https://hacker-news.firebaseio.com/v0/item/${commentId}.json`);
-
-    if (error !== undefined)
-        return (
-            <div className="alert alert-danger" role="alert">
-                {error.message}
-            </div>
-        );
-    if (data === undefined) return <Spinner />;
-
-    return (
-        <React.Fragment>
-            <h1>#{data.id}</h1>
-            <h2>{data.title}</h2>
-            <CardText item={data} />
-            <p>
-                <Badge score={data['score']} />
-                <a href={`https://news.ycombinator.com/item?id=${data['id']}`} target="_blank" className="float-end">
-                    {data['descendants']} Comments
-                </a>
-            </p>
-            <p className="text-muted" style={{overflowX: 'auto', whiteSpace: 'nowrap'}}>
-                <span className="badge rounded-pill bg-secondary">luca</span>{' '}
-                <a href="#" className="badge rounded-pill bg-light text-dark">
-                    matteo
-                </a>
-            </p>
-        </React.Fragment>
-    );
-};
-
 const App = function (props) {
     return (
         <Router>
             <Switch>
                 <Route path="/:commentId">
-                    <Comment />
+                    <CommentPage />
                 </Route>
                 <Route path="/">
                     <Main />
