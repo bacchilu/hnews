19a415f510d3e8ac4b7108fe11c68945a8fda088
Luca Bacchi <bacchilu@gmail.com>
2021-05-27 16:23:19 +0000

    Show user in single comment

--- src/comment_page/index.js
+++ src/comment_page/index.js
@@ -7,6 +7,7 @@ import {useHNItem, CardText} from './utils';
 
 const Comment = function ({item}) {
     const {data, error} = useHNItem(item);
+    const userEl = useRefUserDetails(data?.by);
     const [childComment, setChildComment] = React.useState(null);
     React.useEffect(
         function () {
@@ -27,6 +28,12 @@ const Comment = function ({item}) {
         <React.Fragment>
             <div className="card text-dark bg-light mb-1 shadow rounded">
                 <div className="card-body">
+                    <h6 className="card-subtitle mb-2 text-muted">
+                        <span ref={userEl}>{data.by}</span>
+                        <em className="float-end" title={toLocaleString(data.time * 1000)}>
+                            {relativeTime(data.time * 1000)}
+                        </em>
+                    </h6>
                     <CardText item={data} />
                     <p>
                         <a
