42361206502bf7425987651a8f0aea5a2955b0c2
Luca Bacchi <bacchilu@gmail.com>
2021-05-27 16:13:40 +0000

    Commenta page. A Draft

--- src/comment_page/index.js
+++ src/comment_page/index.js
@@ -1,8 +1,9 @@
-import React, {isValidElement} from 'react';
+import React from 'react';
 import {useParams} from 'react-router-dom';
 import useSWR from 'swr';
 
 import {Spinner, Badge, useRefUserDetails, relativeTime, toLocaleString} from '../utils.js';
+import {CommentersList} from './users';
 
 const useHNItem = function (id) {
     return useSWR(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
@@ -19,62 +20,9 @@ const CardText = function ({item}) {
     );
 };
 
-const UserBadge = function ({item, isItemSelected, setSelected}) {
+const Comment = function ({item}) {
     const {data, error} = useHNItem(item);
-
-    if (error !== undefined) return <span className="badge rounded-pill bg-danger">error</span>;
-    if (data === undefined)
-        return (
-            <div className="spinner-grow spinner-grow-sm" role="status">
-                <span className="visually-hidden">Loading...</span>
-            </div>
-        );
-
-    const onClick = function (e) {
-        e.preventDefault();
-        setSelected();
-    };
-
-    return !isItemSelected ? (
-        <a
-            href={`https://news.ycombinator.com/item?id=${item}`}
-            className="badge rounded-pill bg-light text-dark"
-            onClick={onClick}
-        >
-            {data.by}
-        </a>
-    ) : (
-        <span className="badge rounded-pill bg-secondary">{data.by}</span>
-    );
-};
-
-const CommentersList = function ({kids}) {
-    const getInitDict = function () {
-        return kids.reduce(function (accumulator, currentValue) {
-            return {...accumulator, [currentValue]: false};
-        }, {});
-    };
-    const [selectedKids, setSelectedKids] = React.useState(getInitDict);
-
-    const items = kids.map(function (item) {
-        const setSelected = function () {
-            setSelectedKids({...getInitDict(), [item]: true});
-        };
-
-        return <UserBadge key={item} item={item} isItemSelected={selectedKids[item]} setSelected={setSelected} />;
-    });
-
-    return (
-        <div className="text-muted" style={{overflowX: 'auto', whiteSpace: 'nowrap'}}>
-            {items}
-        </div>
-    );
-};
-
-export const CommentPage = function (props) {
-    const {commentId} = useParams();
-    const {data, error} = useHNItem(commentId);
-    const userEl = useRefUserDetails(data?.by);
+    const [childComment, setChildComment] = React.useState(null);
 
     if (error !== undefined)
         return (
@@ -85,28 +33,67 @@ export const CommentPage = function (props) {
     if (data === undefined) return <Spinner />;
 
     return (
-        <div className="card text-dark bg-light mb-1 shadow rounded">
-            <div className="card-body">
-                <h5 className="card-title">{data.title}</h5>
-                <h6 className="card-subtitle mb-2 text-muted">
-                    <span ref={userEl}>{data.by}</span>
-                    <em className="float-end" title={toLocaleString(data.time * 1000)}>
-                        {relativeTime(data.time * 1000)}
-                    </em>
-                </h6>
-                <CardText item={data} />
-                <p>
-                    <Badge score={data['score']} />
-                    <a
-                        href={`https://news.ycombinator.com/item?id=${data['id']}`}
-                        target="_blank"
-                        className="float-end"
-                    >
-                        {data['descendants']} Comments
-                    </a>
-                </p>
-                <CommentersList kids={data.kids} />
+        <React.Fragment>
+            <div className="card text-dark bg-light mb-1 shadow rounded">
+                <div className="card-body">
+                    <CardText item={data} />
+                    <p>
+                        <a
+                            href={`https://news.ycombinator.com/item?id=${data['id']}`}
+                            target="_blank"
+                            className="float-end"
+                        >
+                            {data.kids !== undefined ? data.kids.length : 0} Comments
+                        </a>
+                    </p>
+                    <CommentersList kids={data.kids || []} selectComment={setChildComment} />
+                </div>
             </div>
-        </div>
+            {childComment !== null && <Comment item={childComment} />}
+        </React.Fragment>
+    );
+};
+
+export const CommentPage = function (props) {
+    const {commentId} = useParams();
+    const {data, error} = useHNItem(commentId);
+    const userEl = useRefUserDetails(data?.by);
+    const [childComment, setChildComment] = React.useState(null);
+
+    if (error !== undefined)
+        return (
+            <div className="alert alert-danger" role="alert">
+                {error.message}
+            </div>
+        );
+    if (data === undefined) return <Spinner />;
+
+    return (
+        <React.Fragment>
+            <div className="card text-dark bg-light mb-1 shadow rounded">
+                <div className="card-body">
+                    <h5 className="card-title">{data.title}</h5>
+                    <h6 className="card-subtitle mb-2 text-muted">
+                        <span ref={userEl}>{data.by}</span>
+                        <em className="float-end" title={toLocaleString(data.time * 1000)}>
+                            {relativeTime(data.time * 1000)}
+                        </em>
+                    </h6>
+                    <CardText item={data} />
+                    <p>
+                        <Badge score={data['score']} />
+                        <a
+                            href={`https://news.ycombinator.com/item?id=${data['id']}`}
+                            target="_blank"
+                            className="float-end"
+                        >
+                            {data['descendants']} Comments
+                        </a>
+                    </p>
+                    <CommentersList kids={data.kids} selectComment={setChildComment} />
+                </div>
+            </div>
+            {childComment !== null && <Comment item={childComment} />}
+        </React.Fragment>
     );
 };
---
+++ src/comment_page/users.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import useSWR from 'swr';
+
+const useHNItem = function (id) {
+    return useSWR(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+};
+
+const UserBadge = function ({item, isItemSelected, setSelected}) {
+    const {data, error} = useHNItem(item);
+
+    if (error !== undefined) return <span className="badge rounded-pill bg-danger">error</span>;
+    if (data === undefined)
+        return (
+            <div className="spinner-grow spinner-grow-sm" role="status">
+                <span className="visually-hidden">Loading...</span>
+            </div>
+        );
+
+    const onClick = function (e) {
+        e.preventDefault();
+        setSelected();
+    };
+
+    return !isItemSelected ? (
+        <a
+            href={`https://news.ycombinator.com/item?id=${item}`}
+            className="badge rounded-pill bg-light text-dark"
+            onClick={onClick}
+        >
+            {data.by}
+        </a>
+    ) : (
+        <span className="badge rounded-pill bg-secondary">{data.by}</span>
+    );
+};
+
+export const CommentersList = function ({kids, selectComment}) {
+    const getInitDict = function () {
+        return kids.reduce(function (accumulator, currentValue) {
+            return {...accumulator, [currentValue]: false};
+        }, {});
+    };
+    const [selectedKids, setSelectedKids] = React.useState(getInitDict);
+
+    const items = kids.map(function (item) {
+        const setSelected = function () {
+            setSelectedKids({...getInitDict(), [item]: true});
+            selectComment(item);
+        };
+
+        return <UserBadge key={item} item={item} isItemSelected={selectedKids[item]} setSelected={setSelected} />;
+    });
+
+    return (
+        <div className="text-muted" style={{overflowX: 'auto', whiteSpace: 'nowrap'}}>
+            {items}
+        </div>
+    );
+};
