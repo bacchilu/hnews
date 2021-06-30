31d26dde5d5c35b40403797b345003e3528de921
Luca Bacchi <bacchilu@gmail.com>
2021-06-30 14:53:20 +0000

    Moved authentication to new Google Account

--- src/model/index.js
+++ src/model/index.js
@@ -1,13 +1,21 @@
 import firebase from 'firebase/app';
 
+// const firebaseConfig = {
+//     apiKey: 'AIzaSyBvHCpKNrfKfc2AZZLP8D0K1AGlUFcX1aE',
+//     authDomain: 'test-budget-38b21.firebaseapp.com',
+//     databaseURL: 'https://test-budget-38b21.firebaseio.com',
+//     projectId: 'test-budget-38b21',
+//     storageBucket: 'test-budget-38b21.appspot.com',
+//     messagingSenderId: '1067130269246',
+//     appId: '1:1067130269246:web:504d76c1afc05bcbf12b6f',
+// };
 const firebaseConfig = {
-    apiKey: 'AIzaSyBvHCpKNrfKfc2AZZLP8D0K1AGlUFcX1aE',
-    authDomain: 'test-budget-38b21.firebaseapp.com',
-    databaseURL: 'https://test-budget-38b21.firebaseio.com',
-    projectId: 'test-budget-38b21',
-    storageBucket: 'test-budget-38b21.appspot.com',
-    messagingSenderId: '1067130269246',
-    appId: '1:1067130269246:web:504d76c1afc05bcbf12b6f',
+    apiKey: "AIzaSyAC-lbWPT-60lPwbJGBMKp_1mLKWCljW4A",
+    authDomain: "hnews-f845a.firebaseapp.com",
+    projectId: "hnews-f845a",
+    storageBucket: "hnews-f845a.appspot.com",
+    messagingSenderId: "734364993587",
+    appId: "1:734364993587:web:f0020375ba6ff35b8efb46"
 };
 firebase.initializeApp(firebaseConfig);
 
