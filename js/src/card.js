7d943432e8dcb9b251f132fa4a875284d544b2cd
Luca Bacchi <bacchilu@gmail.com>
2022-03-16 16:56:10 +0100

    Refactoring

--- js/src/card.js
+++ js/src/card.js
@@ -6,18 +6,18 @@ import {CommentsModal} from './comments';
 import {relativeTime, toLocaleString, Badge, useRefUserDetails} from './utils.js';
 
 const CardText = function ({item}) {
-    if (item['story_text'] !== null) return <em dangerouslySetInnerHTML={{__html: item['story_text']}}></em>;
+    if (item.story_text !== null) return <em dangerouslySetInnerHTML={{__html: item.story_text}}></em>;
+
     return (
         <p className="card-text text-truncate">
-            <a href={item['url']} target="_blank">
-                {item['url']}
+            <a href={item.url} target="_blank">
+                {item.url}
             </a>
         </p>
     );
 };
 
-export const Card = function ({item, inModal}) {
-    const userEl = useRefUserDetails(item['author']);
+const Comments = function ({item, inModal}) {
     const [modalOpened, setModalOpened] = React.useState(false);
 
     const openComments = function (e) {
@@ -27,41 +27,51 @@ export const Card = function ({item, inModal}) {
     };
 
     return (
-        <div className="card text-dark bg-light mb-1 shadow rounded">
+        <>
             <Modal opened={modalOpened} setOpened={setModalOpened}>
                 <CommentsModal item={item} />
             </Modal>
+            <a
+                href={`https://news.ycombinator.com/item?id=${item.objectID}`}
+                className="btn btn-secondary btn-sm position-relative me-4"
+                target="_blank"
+                onClick={openComments}
+            >
+                <i className="bi bi-people"></i>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    {item.num_comments}
+                    <span className="visually-hidden">unread messages</span>
+                </span>
+            </a>
+            <Link to={`/${item.objectID}`} className="btn btn-primary btn-sm position-relative me-4">
+                <i className="bi bi-people-fill"></i>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    {item.num_comments}
+                    <span className="visually-hidden">unread messages</span>
+                </span>
+            </Link>
+        </>
+    );
+};
+
+export const Card = function ({item, inModal}) {
+    const userEl = useRefUserDetails(item.author);
+
+    return (
+        <div className="card text-dark bg-light mb-1 shadow rounded">
             <div className="card-body">
-                <h5 className="card-title">{item['title']}</h5>
+                <h5 className="card-title">{item.title}</h5>
                 <h6 className="card-subtitle mb-2 text-muted">
-                    <span ref={userEl}>{item['author']}</span>
-                    <em className="float-end" title={toLocaleString(item['created_at'])}>
-                        {relativeTime(item['created_at'])}
+                    <span ref={userEl}>{item.author}</span>
+                    <em className="float-end" title={toLocaleString(item.created_at)}>
+                        {relativeTime(item.created_at)}
                     </em>
                 </h6>
                 <CardText item={item} />
                 <p>
-                    <Badge score={item['points']} />
+                    <Badge score={item.points} />
                     <span className="float-end">
-                        <a
-                            href={`https://news.ycombinator.com/item?id=${item['objectID']}`}
-                            className="btn btn-secondary btn-sm position-relative me-4"
-                            target="_blank"
-                            onClick={openComments}
-                        >
-                            <i className="bi bi-people"></i>
-                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                                {item['num_comments']}
-                                <span className="visually-hidden">unread messages</span>
-                            </span>
-                        </a>
-                        <Link to={`/${item['objectID']}`} className="btn btn-primary btn-sm position-relative me-4">
-                            <i className="bi bi-people-fill"></i>
-                            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                                {item['num_comments']}
-                                <span className="visually-hidden">unread messages</span>
-                            </span>
-                        </Link>
+                        <Comments item={item} inModal={inModal} />
                     </span>
                 </p>
             </div>
