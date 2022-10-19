ff52aaac0ae20ade42f045f52f9b762ca42a7997
Luca Bacchi <bacchilu@gmail.com>
2022-10-19 16:18:36 +0200

    Refactoring

--- src/js/fetcher.js
+++ src/js/fetcher.js
@@ -2,25 +2,6 @@ import useSWR from 'swr';
 
 // https://github.com/minimaxir/hacker-news-undocumented
 
-// const ajaxGet = async function (start, end, hitsPerPage) {
-//     const searchParams = new URLSearchParams({
-//         query: '',
-//         numericFilters: `created_at_i>${start},created_at_i<=${end},points>=100`,
-//         hitsPerPage: hitsPerPage,
-//     });
-//     const url = `https://hn.algolia.com/api/v1/search?${searchParams.toString()}`;
-//     return await (await fetch(url)).json();
-// };
-
-// const getData = async function () {
-//     const NOW = new Date() / 1000;
-//     const DAY = 60 * 60 * 24;
-//     const start = NOW - 3 * DAY;
-//     const end = NOW;
-//     const res = await ajaxGet(start, end, 0);
-//     return (await ajaxGet(start, end, res['nbHits']))['hits'];
-// };
-
 const Fetch = (function () {
     const ajaxGet = async function (start, end, hitsPerPage) {
         const searchParams = new URLSearchParams({
