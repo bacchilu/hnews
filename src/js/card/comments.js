3fbaafa8e736b3008d9aeb1204bea155cef5f5ee
Luca Bacchi <bacchilu@gmail.com>
2022-10-19 15:23:00 +0200

    Bug fix

--- src/js/card/comments.js
+++ src/js/card/comments.js
@@ -1,54 +1,25 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 
-// import {Modal} from '../libs/modal.js';
-// import {CommentsModal} from '../comments';
-
-// export const Comments = function ({item, inModal}) {
-//     const [modalOpened, setModalOpened] = React.useState(false);
-
-//     const openComments = function (e) {
-//         if (inModal) return;
-//         e.preventDefault();
-//         setModalOpened(true);
-//     };
-
-//     return (
-//         <>
-//             <Modal opened={modalOpened} setOpened={setModalOpened}>
-//                 <CommentsModal item={item} />
-//             </Modal>
-//             <a
-//                 href={`https://news.ycombinator.com/item?id=${item.objectID}`}
-//                 className="btn btn-secondary btn-sm position-relative me-4"
-//                 target="_blank"
-//                 onClick={openComments}
-//             >
-//                 <i className="bi bi-people"></i>
-//                 <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-//                     {item.num_comments}
-//                     <span className="visually-hidden">unread messages</span>
-//                 </span>
-//             </a>
-//             <Link to={`/${item.objectID}`} className="btn btn-primary btn-sm position-relative me-4">
-//                 <i className="bi bi-people-fill"></i>
-//                 <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-//                     {item.num_comments}
-//                     <span className="visually-hidden">unread messages</span>
-//                 </span>
-//             </Link>
-//         </>
-//     );
-// };
+const LinkTo = function ({to, disabled, children}) {
+    return disabled ? (
+        <Link to={`/${to}`} style={{pointerEvents: 'none'}}>
+            {children}
+        </Link>
+    ) : (
+        <Link to={`/${to}`}>{children}</Link>
+    );
+};
 
 export const Comments = function ({item}) {
     return (
-        <Link to={`/${item.objectID}`} className="btn btn-primary btn-sm position-relative me-4">
-            <i className="bi bi-people-fill"></i>
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                {item.num_comments}
-                <span className="visually-hidden">unread messages</span>
+        <LinkTo to={`/${item.objectID}`} disabled={item.num_comments === null}>
+            <span className="btn btn-primary btn-sm position-relative me-4">
+                <i className="bi bi-people-fill"></i>
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                    {item.num_comments}
+                </span>
             </span>
-        </Link>
+        </LinkTo>
     );
 };
