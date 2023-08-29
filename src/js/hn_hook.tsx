a3b710ad320849f9be4f41c5bb9996a6e9b81936
Luca Bacchi <bacchilu@gmail.com>
2023-08-29 17:07:45 +0200

    Working on progress cb

--- src/js/hn_hook.tsx
+++ src/js/hn_hook.tsx
@@ -13,6 +13,16 @@ export interface HNItem {
     num_comments: number | null;
 }
 
+const createCounter = function () {
+    let progress = 0;
+    return {
+        incr: function () {
+            progress += 1;
+            return progress;
+        },
+    };
+};
+
 const Fetch = (function () {
     const getDaysHits = async function (start: number, end: number, hitsPerPage: number, cb: () => void) {
         const searchParams = new URLSearchParams({
@@ -28,31 +38,36 @@ const Fetch = (function () {
     };
 
     return {
-        getData: async function () {
-            const NOW = Date.now() / 1000;
-            const DAY = 60 * 60 * 24;
+        getData: function (progressCb: (data: [number, number]) => void) {
+            return async () => {
+                const NOW = Date.now() / 1000;
+                const DAY = 60 * 60 * 24;
 
-            const data = [0, 1, 2, 3, 4, 5, 6];
-            let progress = 0;
-            const cb = function () {
-                progress += 1;
-                console.log(progress);
+                const data = [0, 1, 2, 3, 4, 5, 6];
+                const counter = createCounter();
+                const cb = function () {
+                    const res: [number, number] = [counter.incr(), data.length];
+                    progressCb(res);
+                };
+                const res = await Promise.all(
+                    data.map((i) => getDaysHits(NOW - (7 - i) * DAY, NOW - (6 - i) * DAY, 2 ** i, cb))
+                );
+
+                return res
+                    .reduce((acc, item) => [...acc, ...item], [])
+                    .sort((a, b) => {
+                        if (a.points < b.points) return 1;
+                        if (a.points > b.points) return -1;
+                        return 0;
+                    });
             };
-            const res = await Promise.all(
-                data.map((i) => getDaysHits(NOW - (7 - i) * DAY, NOW - (6 - i) * DAY, 2 ** i, cb))
-            );
-
-            return res
-                .reduce((acc, item) => [...acc, ...item], [])
-                .sort((a, b) => {
-                    if (a.points < b.points) return 1;
-                    if (a.points > b.points) return -1;
-                    return 0;
-                });
         },
     };
 })();
 
 export const useHNItems = function () {
-    return useSWR('HN_ITEMS', Fetch.getData, {dedupingInterval: 60000});
+    const progressCb = function (data: [number, number]) {
+        console.log(data);
+    };
+    return useSWR('HN_ITEMS', Fetch.getData(progressCb), {dedupingInterval: 60000});
 };
