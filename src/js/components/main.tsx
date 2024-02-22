b0ac8b73fe94641fa67a2157049ce13fbbb0d48b
Luca Bacchi <bacchilu@gmail.com>
2024-02-22 16:55:53 +0100

    Testing a new ordering strategy

--- src/js/components/main.tsx
+++ src/js/components/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import {useHNItems} from '../hooks/hn_hook';
+import {HNItem} from '../model/hn';
 import {Card} from './card';
 import {Spinner} from './spinner';
 
@@ -12,17 +13,41 @@ const ErrorAlert: React.FC<{message: string}> = function ({message}) {
     );
 };
 
+const CardList: React.FC<{hnItems: HNItem[]}> = function ({hnItems}) {
+    return hnItems.map((item) => <Card key={item.objectID} item={item} />);
+};
+
+const CardList2: React.FC<{hnItems: HNItem[]}> = function ({hnItems}) {
+    const now = new Date();
+    const filterLastNDays = function (hnItem: HNItem, days: number) {
+        const DAY = 24 * 60 * 60 * 1000;
+        const milliseconds = now.getTime() - hnItem.created_at.getTime();
+        return milliseconds <= days * DAY && milliseconds > (days - 1) * DAY;
+    };
+
+    return [1, 2, 3, 4, 5, 6, 7].map((day, index, array) => {
+        const isLast = index + 1 === array.length;
+        const filteredHNItems = hnItems.filter((hnItem) => filterLastNDays(hnItem, day));
+        return (
+            <div key={day}>
+                <CardList hnItems={filteredHNItems} />
+                {!isLast && <hr style={{margin: '2rem'}} />}
+            </div>
+        );
+    });
+};
+
 export const Main: React.FC<{recents: boolean; setRecents: (v: boolean) => void}> = function ({recents, setRecents}) {
-    const {data, error} = useHNItems(recents ? 1 : 7);
+    const {data: hnItems, error} = useHNItems(recents ? 1 : 7);
 
     const changeRecents = function (e: React.ChangeEvent<HTMLInputElement>) {
         setRecents(e.target.checked);
     };
 
     if (error !== undefined) return <ErrorAlert message={error.message} />;
-    if (data === undefined) return <Spinner />;
+    if (hnItems === undefined) return <Spinner />;
 
-    const items = data.map((item) => <Card key={item.objectID} item={item} />);
+    const items = <CardList2 hnItems={hnItems} />;
     return (
         <>
             <div className="form-check form-switch">
