ee495f77a922449e7f9770a1a49026444ce21002
Luca Bacchi <bacchilu@gmail.com>
2024-11-07 17:42:43 +0100

    Testing zod

--- src/ts/model/hn.ts
+++ src/ts/model/hn.ts
@@ -1,16 +1,38 @@
 // https://hn.algolia.com/api
 // https://github.com/minimaxir/hacker-news-undocumented
 
+import {z} from 'zod';
+
+const HNItemParser = z.object({
+    objectID: z.string(),
+    author: z.string(),
+    title: z.string(),
+    points: z.number().int().nonnegative(),
+    created_at: z
+        .string()
+        .datetime()
+        .transform((v) => new Date(v)),
+    story_text: z.string().optional(),
+    url: z.string().optional(),
+    num_comments: z.number().int().nonnegative(),
+});
+
 export interface HNItem {
     objectID: string;
     author: string;
     title: string;
     points: number;
     created_at: Date;
-    story_text: string;
-    url: string;
-    num_comments: number | null;
+    story_text?: string;
+    url?: string;
+    num_comments: number;
 }
+
+type AssertSameType<T, U> = T extends U ? (U extends T ? true : never) : never;
+type Test = AssertSameType<z.infer<typeof HNItemParser>, HNItem>;
+const testValue: Test = true;
+
+const HNItemsParser = z.array(HNItemParser);
 
 export const getHNItems = async function (from: number, to: number, hitsPerPage: number) {
     const searchParams = new URLSearchParams({
@@ -22,14 +44,5 @@ export const getHNItems = async function (from: number, to: number, hitsPerPage:
     const res = await fetch(url);
     if (!res.ok) throw new Error('An error occurred while fetching the data.');
     const data: any[] = (await res.json()).hits;
-    return data.map((item) => ({
-        objectID: item.objectID,
-        author: item.author,
-        title: item.title,
-        points: item.points,
-        created_at: new Date(item.created_at),
-        story_text: item.story_text,
-        url: item.url,
-        num_comments: item.num_comments,
-    })) as HNItem[];
+    return HNItemsParser.parse(data);
 };
