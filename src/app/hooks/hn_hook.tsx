abc4cfb3504c18b3a0c7709508105b4826121011
Luca Bacchi <bacchilu@gmail.com>
2025-11-29 14:37:45 +0100

    refactor: simplify HN items flattening with flat()

--- src/app/hooks/hn_hook.tsx
+++ src/app/hooks/hn_hook.tsx
@@ -17,13 +17,11 @@ export const useHNItems = function (dataGateway: HNItemsGateway): {
         const res: HNItem[][] = await Promise.all(
             [0, 1, 2, 3, 4, 5, 6].map((i) => dataGateway.getData(NOW - (7 - i) * DAY, NOW - (6 - i) * DAY, 2 ** i))
         );
-        return res
-            .reduce((acc, item) => [...acc, ...item], [])
-            .sort((a, b) => {
-                if (a.points < b.points) return 1;
-                if (a.points > b.points) return -1;
-                return 0;
-            });
+        return res.flat().sort((a, b) => {
+            if (a.points < b.points) return 1;
+            if (a.points > b.points) return -1;
+            return 0;
+        });
     };
 
     const {data, error} = useSWR<HNItem[], Error>('HN_ITEMS', fetcher, {dedupingInterval: 60000});
