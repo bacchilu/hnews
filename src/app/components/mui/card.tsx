5ad96966d851307ed8b4acdf8b04094baddba955
Luca Bacchi <bacchilu@gmail.com>
2026-09-01 12:57:29 +0200

    Use a lighter background for selected comments

--- src/app/components/mui/card.tsx
+++ src/app/components/mui/card.tsx
@@ -16,7 +16,7 @@ const Card: React.FC<{
             component="article"
             sx={{
                 alignItems: 'start',
-                bgcolor: selected ? 'action.selected' : undefined,
+                bgcolor: selected ? 'grey.50' : undefined,
                 borderLeftColor: selected ? 'primary.main' : isFresh ? 'success.main' : 'divider',
                 borderLeftStyle: 'solid',
                 borderLeftWidth: selected || isFresh ? 4 : 2,
@@ -27,7 +27,7 @@ const Card: React.FC<{
                 px: 2,
                 py: 1.5,
                 transition: (theme) => theme.transitions.create('background-color'),
-                '&:hover': {bgcolor: selected ? 'action.selected' : 'action.hover'},
+                '&:hover': {bgcolor: selected ? 'grey.50' : 'action.hover'},
             }}
         >
             {hasMarker && <Box sx={{gridColumn: 1, gridRow: 1, pt: 0.25}}>{marker}</Box>}
